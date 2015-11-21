6b0171142826fe977c83719479842634a10de5c5
Assistanz <Assistanz@d1.assistanz.local>
2015-11-21 15:09:57 +0530

    Active user count issue fixed in admin user list page.

--- src/main/webapp/scripts/controllers/client/user.js
+++ src/main/webapp/scripts/controllers/client/user.js
@@ -8,17 +8,29 @@ angular
         .controller('userListCtrl', userListCtrl)
 
 function userListCtrl($scope, crudService) {
-
 	$scope.paginationObject = {};
-	$scope.active = {};
-	$scope.inActive = {};
-
     // User List
     $scope.list = function (pageNumber) {
-    	var active = 0;
-    	var inActive = 0;
         var limit = (angular.isUndefined($scope.paginationObject.limit)) ? $scope.global.CONTENT_LIMIT : $scope.paginationObject.limit;
         var hasUsers = crudService.list("users", $scope.global.paginationHeaders(pageNumber, limit), {"limit": limit});
+        hasUsers.then(function (result) {  // this is only run after $http completes0
+            $scope.accountList = result;
+            $scope.paginationObject.limit  = limit;
+            $scope.paginationObject.currentPage = pageNumber;
+            $scope.paginationObject.totalItems = result.totalItems;
+            $scope.totalUser = result.totalItems;
+        });
+    };
+    $scope.list(1);
+
+    $scope.active = {};
+	$scope.inActive = {};
+
+    // User List for count active user
+    $scope.list = function () {
+    	var active = 0;
+    	var inActive = 0;
+        var hasUsers = crudService.listAll("users/list");
         hasUsers.then(function (result) {  // this is only run after $http completes0
         	angular.forEach(result, function(value, key) {
             	if (value.isActive)
@@ -28,12 +40,7 @@ function userListCtrl($scope, crudService) {
             });
         	$scope.active = active;
         	$scope.inActive = inActive;
-            $scope.accountList = result;
-            $scope.paginationObject.limit  = limit;
-            $scope.paginationObject.currentPage = pageNumber;
-            $scope.paginationObject.totalItems = result.totalItems;
-            $scope.totalUser = result.totalItems;
         });
     };
-    $scope.list(1);
+    $scope.list();
 };
