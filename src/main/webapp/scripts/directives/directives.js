1ae3b052da082a9fe38db9058ccb8167000d7638
Mohanapriya <Mohanapriya@assistanz.com>
2016-07-04 17:10:48 +0530

    quick added in activity and button config done

--- src/main/webapp/scripts/directives/directives.js
+++ src/main/webapp/scripts/directives/directives.js
@@ -722,7 +722,7 @@ function validCharacters() {
                 if (angular.isUndefined(val)) {
                     var val = 0;
                 }
-                var clean = val.replace(/[^0-9.*!@$A-Za-z-_\s\u4e00-\u9fa5]/g, '');
+                var clean = val.replace(/[^0-9.:,=*!@$A-Za-z-_\s\u4e00-\u9fa5]/g, '');
                 if (clean < parseInt(attrs.ngMin)) {
                      clean = clean.substring(1, clean.length);
                 }
